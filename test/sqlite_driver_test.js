e099a1c5dfad922f9c27ac3604b121da3f7327de
okunishinishi <okunishinishi@gmail.com>
2017-08-30 08:56:55 +0900

    versionup

--- test/sqlite_driver_test.js
+++ test/sqlite_driver_test.js
@@ -6,7 +6,7 @@
 
 const clayDriverTests = require('clay-driver-tests')
 const SqliteDriver = require('../lib/sqlite_driver.js')
-const { ok, equal, deepEqual } = require('assert')
+const {ok, equal, deepEqual} = require('assert')
 const clayLump = require('clay-lump')
 const co = require('co')
 const filedel = require('filedel')
@@ -51,17 +51,17 @@ describe('sqlite-driver', function () {
     equal(updated.password, 'hogehoge')
 
     let list01 = await driver.list('users', {})
-    deepEqual(list01.meta, { offset: 0, limit: 100, length: 2, total: 2 })
+    deepEqual(list01.meta, {offset: 0, limit: 100, length: 2, total: 2})
 
     let list02 = await driver.list('users', {
-      filter: { username: 'okunishinishi' }
+      filter: {username: 'okunishinishi'}
     })
-    deepEqual(list02.meta, { offset: 0, limit: 100, length: 1, total: 1 })
+    deepEqual(list02.meta, {offset: 0, limit: 100, length: 1, total: 1})
 
     let list03 = await driver.list('users', {
-      page: { size: 1, number: 1 }
+      page: {size: 1, number: 1}
     })
-    deepEqual(list03.meta, { offset: 0, limit: 1, length: 1, total: 2 })
+    deepEqual(list03.meta, {offset: 0, limit: 1, length: 1, total: 2})
 
     let destroyed = await driver.destroy('users', one.id)
     equal(destroyed, 1)
@@ -89,11 +89,11 @@ describe('sqlite-driver', function () {
     })
     let User = lump.resource('user')
     await User.drop()
-    await User.create({ name: 'hoge' })
-    let user = await User.first({ name: 'hoge' })
+    await User.create({name: 'hoge'})
+    let user = await User.first({name: 'hoge'})
     let destroyed = await User.destroy(user.id)
     equal(destroyed, 1)
-    let mustBeNull = await User.first({ name: 'hoge' })
+    let mustBeNull = await User.first({name: 'hoge'})
     ok(!mustBeNull)
   })
 
@@ -107,7 +107,7 @@ describe('sqlite-driver', function () {
       })
     })
     let Person = lump.resource('Person')
-    await Person.createBulk([ {
+    await Person.createBulk([{
       pid: 1,
       name: 'a',
       age: 2
@@ -123,18 +123,18 @@ describe('sqlite-driver', function () {
       pid: 2,
       name: 'd',
       age: 6
-    } ])
+    }])
 
     {
-      let people = await Person.list({ filter: { pid: 1 }, sort: [ 'age' ] })
+      let people = await Person.list({filter: {pid: 1}, sort: ['age']})
       let ages = people.entities.map(p => p.age)
-      deepEqual(ages, [ 1, 2, 3 ])
+      deepEqual(ages, [1, 2, 3])
     }
 
     {
-      let people = await Person.list({ filter: { pid: 1 }, sort: [ '-age' ] })
+      let people = await Person.list({filter: {pid: 1}, sort: ['-age']})
       let ages = people.entities.map(p => p.age)
-      deepEqual(ages, [ 3, 2, 1 ])
+      deepEqual(ages, [3, 2, 1])
     }
   })
 
@@ -145,32 +145,32 @@ describe('sqlite-driver', function () {
       bar: {
         b: false,
         n: 1,
-        s: 'hoge',
-        d
-      }
+        s: 'hoge'
+      },
+      d
     })
     equal(typeof created.bar.b, 'boolean')
     equal(typeof created.bar.n, 'number')
     equal(typeof created.bar.s, 'string')
-    ok(created.bar.d instanceof Date)
+    ok(created.d instanceof Date)
 
     await driver.drop('Foo')
     await driver.create('User', {
       name: 'user01',
-      org: { $ref: 'Org#1' }
+      org: {$ref: 'Org#1'}
     })
     await driver.create('User', {
       name: 'user02',
-      org: { $ref: 'Org#2' }
+      org: {$ref: 'Org#2'}
     })
 
     let list = await driver.list('User', {
       filter: {
-        org: { $ref: 'Org#2' }
+        org: {$ref: 'Org#2'}
       }
     })
     equal(list.meta.length, 1)
-    equal(list.entities[ 0 ].name, 'user02')
+    equal(list.entities[0].name, 'user02')
 
     await driver.drop('User')
   })
